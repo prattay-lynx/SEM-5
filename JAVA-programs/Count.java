1b2dbccd7cd17ac28219bd9ffe7a7be66684878f
Prattay <prattaypaul2@gmail.com>
2022-07-13 17:57:24 +0530

    Java-programs added

---
+++ JAVA-programs/Count.java
@@ -0,0 +1,12 @@
+public class Count {
+    public static void main(String []args) {
+        int a = Integer.parseInt(args[0]);
+        while(a !=  0)
+        {
+            System.out.print(args[1]+" ");
+            a=a-1;
+        }
+        
+    }
+    
+}
