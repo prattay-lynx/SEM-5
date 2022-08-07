5640f27455c11593782ef76848876c23fc6ea2f6
Prattay <prattaypaul2@gmail.com>
2022-08-07 17:11:32 +0530

    Basic Linux Commands

---
+++ Linux_Commands/Commands1.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+dir #lists the files in a directory
+
+hostname #Displays or changes the hostname of the system
+
+df -m #displays the available disk space in each partition
+
+du #use du to know the disk usage of a file in the system
+
+which $BASH # to know where the bash shell is located in ur distro
+
+locate -i Commands.txt #to locate or search a file in ur file system ,-i to ignore the cases
+
+uname #use uname to show information of ur linux distro
+
+read -p "Enter ur name:" names1 #to read name in the same line
+echo $names1
+
+read -sp "Enter password: " password #to take silent input
+echo $password
+
+ping google.com #ping is used to check one's coonection to a server
+#ctrl + c to get out
+
+cal #to check calendar
+
+
+
+
+
